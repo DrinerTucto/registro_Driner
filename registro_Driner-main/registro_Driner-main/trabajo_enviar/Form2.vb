d9233d4c71337fd626c0044a9a6e501b09d2cde1
DrinerTucto <1304745@senati.pe>
2022-10-23 18:49:04 -0500

    esto es el 80% del trabajop avanzado

---
+++ registro_Driner-main/registro_Driner-main/trabajo_enviar/Form2.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form2
+
+
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+
+    Private Sub CrystalReportViewer1_Load_1(sender As Object, e As EventArgs) Handles CrystalReportViewer1.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
